80a12a3abb6cfb0b8b407eb32f463209ce8f5c66
aerain <j40f893@gmail.com>
2019-05-06 20:33:04 +0900

    login

--- app/src/main/java/kr/ac/jejunu/giftapplication/login/LoginActivity.java
+++ app/src/main/java/kr/ac/jejunu/giftapplication/login/LoginActivity.java
@@ -1,16 +1,74 @@
 package kr.ac.jejunu.giftapplication.login;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import kr.ac.jejunu.giftapplication.R;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private EditText email, password;
+    private LinearLayout loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton.setOnClickListener(this::tryLogin);
     }
 
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        toolbar = findViewById(R.id.login_toolbar);
+        loginButton = findViewById(R.id.login_login_button);
+        email = findViewById(R.id.login_id_editText);
+        password = findViewById(R.id.login_pwd_editText);
+        setToolBar();
+    }
+
+    private void setToolBar() {
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setHomeAsUpIndicator(R.drawable.arrow_back);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void tryLogin(View v) {
+        String emailString = email.getText().toString().trim();
+        String passwordString = password.getText().toString().trim();
+
+        if(!(emailString.equals("") && passwordString.equals(""))) {
+            Intent intent = new Intent(this, LoginLoadingActivity.class);
+            intent.putExtra("email", emailString);
+            intent.putExtra("password", passwordString);
+            startActivity(intent);
+        }
+    }
 }
---
+++ app/src/main/java/kr/ac/jejunu/giftapplication/login/LoginLoadingActivity.java
@@ -0,0 +1,50 @@
+package kr.ac.jejunu.giftapplication.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+import kr.ac.jejunu.giftapplication.R;
+import kr.ac.jejunu.giftapplication.home.MainActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class LoginLoadingActivity extends AppCompatActivity {
+    private String email;
+    private String password;
+
+    // 이거는 임시 이메일 패스워드;
+    private final String tempEmail = "foo@ex";
+    private final String tempPassword = "1234";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_loading);
+        getExtra();
+
+        Intent resultIntent = new Intent();
+        new Handler().postDelayed(() -> {
+
+        }, 1500);
+        Snackbar.make(getWindow().getDecorView().getRootView(), "넌 틀려먹었어", Snackbar.LENGTH_SHORT).show();
+        if(email.equals(tempEmail) && password.equals(tempPassword)) {
+            // Todo 로그인 로직 구현하기! 이부분!
+
+        } else {
+            finish();
+        }
+
+    }
+
+    private void getExtra() {
+        Intent prevIntent = getIntent();
+        email = prevIntent.getStringExtra("email");
+        password = prevIntent.getStringExtra("password");
+    }
+
+    @Override
+    public void onBackPressed() {
+    }
+}
